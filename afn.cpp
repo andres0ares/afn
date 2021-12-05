dbb5a2946884b7d6cd8c518ef57c41592e65bfc3
Andre soares <andreiarley@gmail.com>
2021-12-05 17:39:50 -0300

    working v0.1

--- afn.cpp
+++ afn.cpp
@@ -103,44 +103,70 @@ void getDescricao(void) {
     MyReadFile.close();
 }
 
-int main(void) {
 
-    
-
-    cout << "===== SIMULACAO AUTOMATO FINITO NAO-DETERMINISTICO =====";
-    getDescricao();
-
-    if(error == 0) {
-        return 0;
-    }
-
-    cout << "\n\n---- DADOS LIDOS ----\n\n";
-
-    for(int j = 0; j < 4; j++){
-        cout << "\n" << els[j];
-        for(int i = 0; i < elements[j].amountData; i++){
-            cout << "\n-> " << elements[j].data[i]; 
+// verifica se ha um simbolo que nao faz parte do alfabeto
+int fazParteAlfabeto(void) {
+    for(int i = 0; i < cadeia.length(); i++){
+        string simbolo(1, cadeia[i]);
+        int count = 0;
+        for(int j = 0; j < elements[0].amountData; j++){
+            if(simbolo == elements[0].data[j]){
+                count++;
+            }
+        }
+        if(count == 0){
+            return 0;
         }
     }
 
-    cout << "\n\n---- TRANSICOES ----\n\n";
+    return 1;
+}
+
+
+int main(void) {
+
+
+    cout << "\n\n ===== SIMULACAO AUTOMATO FINITO NAO-DETERMINISTICO =====";
+
+    // Lê dados do arquivo descricao.txt | encerra programa se contem erro no arquivo.
+    getDescricao();
+    if(error == 0) {
+        return -1;
+    }
+
+    // *********************** exibe dados da descricao.txt  ***************************
+
+    cout << "\n\n ---- DADOS LIDOS (descricao.txt) ----\n\n";
+
+    for(int j = 0; j < 4; j++){
+        cout << "\n " << els[j];
+        for(int i = 0; i < elements[j].amountData; i++){
+            cout << "\n -> " << elements[j].data[i]; 
+        }
+    }
 
     for(int i = 0; i < qtd_transicoes; i++) {
-        cout << "\ntransicao " << i+1;
-        cout << "\nde: " << transicoes[i].from;
+        cout << "\n transicao " << i+1;
+        cout << "\n de: " << transicoes[i].from;
         cout << ", para: " << transicoes[i].to;
         cout << ", simbolo: " << transicoes[i].symbol;
     }
 
-    cout << "\n\nPara proseguir digite a cadeia, e precione enter: ";
+    cout << "\n\n Para proseguir digite a cadeia, e precione enter: ";
     cin >> cadeia;
 
-    cout << "\n\n===== INICIO DA SIMULACAO =====";
-    cout << "\n\nCadeia inserida -> " << cadeia;
-
-    
-    
+    cout << "\n\n ===== INICIO DA SIMULACAO =====";
+    cout << "\n\n Cadeia inserida -> " << cadeia;    
     cout << "\n\n";
+
+
+    // verifica se a cadeia contem um simbolo que nao faz parte do alfabeto
+
+    if(fazParteAlfabeto() == 0){
+        cout << "\n error: cadeia possui simbolo que nao faz parte do alfabeto. \n";
+        return -1;
+    }
+
 
     // *********************** simulacao ***************************
 
@@ -150,9 +176,8 @@ int main(void) {
     // elements[3] = estados finais
 
 
-    // fazParteAlfabeto();
     
-    string* estado = new string [elements[1].amountData];
+    string* estado = new string [elements[1].amountData]; 
     string* newEstado = new string [elements[1].amountData];
     int qtd_estado = 1; 
     int qtd_newEstado = 0;
@@ -162,18 +187,25 @@ int main(void) {
 
     for(int i = 0; i < cadeia.length(); i++){
 
-        cout << "\nCaracter lido " << cadeia[i];
+
+        cout << "\n\n ------- Caractere " << cadeia[i];
+        cout << "-------";
 
         string simbolo(1, cadeia[i]);
         int count = 0;
+
+        //verifica se ha possibilidade de transicao
         for(int j = 0; j < qtd_transicoes; j++) {
 
 
             for(int l=0; l < qtd_estado; l++){
                 
+                //se ha, armazena o novo estado no array newEstado
                 if(estado[l] == transicoes[j].from && (simbolo == transicoes[j].symbol)){
-                    cout << "\nindo para " << transicoes[j].to;
+                    cout << "\n de " << estado[l];
+                    cout << ", para " << transicoes[j].to;
 
+                    //evita duplicidade de estados
                     int exist = 1;
                     for(int k = 0; k < qtd_newEstado; k++){
                         if(newEstado[k] == transicoes[j].to)
@@ -191,30 +223,46 @@ int main(void) {
 
         }
 
-        cout << "\n estados: ";
+        //limpa array
         for(int j = 0; j < qtd_estado; j++){
-            cout << " " << estado[j];
             estado[j] = "";
         }
 
-        cout << "\n new estados: ";
+        //atualiza o array estado e limpa o array newEstado
         for(int j = 0; j < qtd_newEstado; j++){
-            cout << " " << newEstado[j];
             estado[j] = newEstado[j];
             newEstado[j] = "";
         }
 
+        //atualiza a quantidade
         qtd_estado = qtd_newEstado;
         qtd_newEstado = 0;
-
-        cout << "\n estados: ";
-        for(int j = 0; j < qtd_estado; j++){
-            cout << " " << estado[j];
-        }
         
     }
 
-    return 0;
+    //verifica de possui um estado final no array estado
+    int aceita = 0;
+    for(int i = 0; i < qtd_estado; i++){
+        for(int j = 0; j < elements[3].amountData; j++){
+            if(estado[i] == elements[3].data[j]){
+                if(aceita == 0)
+                    cout << "\n\n ===========  CADEIA ACEITA ===============\n"
+                ;
+                cout << "\n Aceita no estado: " << estado[i];
+                aceita++;
+            }
+                
+        }
+    }
+
+    if(aceita == 0)
+        cout << "\n\n ===========  CADEIA REJEITADA ===============\n"
+    ;
+
+    cout << "\n";
+
+
+    return -1;
 
     // ******************* fim simulacao ***************************
     
